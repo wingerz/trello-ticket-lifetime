1f72901b92895b62ede5b2f9a83b82a5165936f6
Wing Yung <wing@yelp.com>
2014-01-18 06:04:58 -0800

    basic card fetching and processing

---
+++ fetch.py
@@ -0,0 +1,35 @@
+import json
+import trello
+import yaml
+
+
+
+def get_client(config):
+    client = trello.TrelloClient(config['key'], api_secret=config['secret'], token=config['oauth_token'], token_secret=config['oauth_token_secret'])
+    return client
+
+def card_to_json(card):
+    card.fetch()
+    card.fetch_actions(action_filter='all')
+    card_actions = card.actions
+    return_dict = {}
+    for key in ['id', 'closed', 'description', 'due', 'labels', 'list_id', 'member_ids', 'name', 'short_id', 'url']:
+        return_dict[key] = getattr(card, key)
+    return_dict['actions'] = card_actions
+    return return_dict
+
+DATA_DIR = 'data/'
+    
+if __name__ == "__main__":
+    with open('secret.yaml') as f:
+        config = yaml.load(f)
+
+
+    client = get_client(config)
+    boards = client.list_boards()
+    mobile_board = client.get_board('51bba780ac3b4aae26006928')
+
+    cards = mobile_board.all_cards()
+    for card in cards:
+        with open(DATA_DIR + card.id + '.json', 'w') as f:
+            print >>f, json.dumps(card_to_json(card))
---
+++ process_cards.py
@@ -0,0 +1,39 @@
+import json
+import sys
+
+def get_action_list(card):
+    events = []
+    for action in card['actions']:
+        if action['type'] == 'createCard':
+            create_event = {
+                'date': action['date'],
+                'after_list': action['data']['list'],
+            }
+            events.append(create_event)
+        elif action['type'] == 'updateCard':
+            move_event = {
+                'date': action['date'],
+                'after_list': action['data']['listAfter']
+            }
+            events.append(move_event)
+
+    events = sorted(events, key=lambda(x): x['date'])
+    return events
+    
+def transform_card(card):
+    return_dict = {}
+    for key in ['id', 'url', 'name']:
+        return_dict[key] = card[key]
+
+    return_dict['lifetime'] = get_action_list(card)
+    return return_dict
+    
+
+if __name__ == "__main__":
+    card_file = sys.argv[1]
+    with open(card_file, 'r') as f:
+        data = f.readline()
+        card = json.loads(data)
+
+    new_card = transform_card(card)
+    print json.dumps(new_card)
