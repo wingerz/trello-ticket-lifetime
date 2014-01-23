e7014136134eb901e6171bd90732d6887da40173
Wing Yung <wing@yelp.com>
2014-01-23 12:36:26 -0800

    different rendering, calculate some stats

--- construct_lanes.py
+++ construct_lanes.py
@@ -1,7 +1,11 @@
 import json
 from os import listdir
 from os.path import isfile, join
+import pprint
 import sys
+
+import dateutil.parser
+import numpy
 
 
 def list_to_class(list_name):
@@ -16,6 +20,39 @@ def list_to_class(list_name):
     else:
         return 'queued'
 
+def is_complete(card):
+    for phase in card['lifetime']:
+        phase_after_list = phase.get('after_list', {}).get('name')
+        phase_after_list_type = list_to_class(phase_after_list) if phase_after_list else None
+        
+        if phase_after_list_type == 'done':
+            return True
+    return False
+
+
+def get_phase_time(card, phase_type):
+    phase_time = 0
+    phase_start_time = None
+    for phase in card['lifetime']:
+        phase_after_list = phase.get('after_list', {}).get('name')
+        phase_after_list_type = list_to_class(phase_after_list) if phase_after_list else None
+        
+        if phase_start_time:
+            phase_end_time = dateutil.parser.parse(phase['date'])
+            phase_time += (phase_end_time - phase_start_time).total_seconds()
+            phase_start_time = None
+        
+        if phase_after_list_type == phase_type:
+            phase_start_time = dateutil.parser.parse(phase['date'])
+
+    return phase_time / 24. / 3600
+
+def summarize_card_times(card):
+    queue_time = get_phase_time(card, 'queued')
+    dev_time = get_phase_time(card, 'dev')
+    review_time = get_phase_time(card, 'review')
+    ready_time = get_phase_time(card, 'ready')
+    return queue_time, dev_time, review_time, ready_time
 
 def construct_lanes(cards):
     lanes = []
@@ -65,7 +102,43 @@ def construct_lanes(cards):
         'lists': list(lists)
     }
     
+def compute_stats(arr):
+    return {
+        'count': len(arr),
+        'avg': numpy.average(arr),
+        'median': numpy.median(arr),
+        'std': numpy.std(arr),
+        'min': min(arr),
+        'max': max(arr),
+    }
+    
 
+def process_times(all_card_times):
+    queue_times = [card[1] for card in all_card_times]
+    dev_times = [card[2] for card in all_card_times]
+    review_times = [card[3] for card in all_card_times]
+    ready_times = [card[4] for card in all_card_times]
+
+    total_time_in_flight = [card[1] + card[2] + card[3] + card[4] for card in all_card_times]
+    dev_to_review_ratio = []
+
+    for card in all_card_times:
+        if card[2] > 0.5 and card[3] > 0.5:
+            dev_to_review_ratio.append(card[2] / card[3])
+
+    stats = {
+        'total': compute_stats(total_time_in_flight),
+        'review': compute_stats(review_times),
+        'dev': compute_stats(dev_times),
+        'ready': compute_stats(ready_times),
+        'queue': compute_stats(queue_times),
+        'dev_to_review_ratio': compute_stats(dev_to_review_ratio)
+    }
+    return stats
+
+
+    
+    
 if __name__ == "__main__":
     input_dir = sys.argv[1]
     data_files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
@@ -76,4 +149,23 @@ if __name__ == "__main__":
         card = json.loads(data)
         cards.append(card)
     output = construct_lanes(cards)
-    print json.dumps(output)
+    with open('lanes.json', 'w') as f:
+        print >>f, json.dumps(output)
+
+
+    card_time_data = []
+    for card in cards:
+        if is_complete(card):
+            data = [card['name'].replace(',', ' ')]
+            times = summarize_card_times(card)
+            data.extend(times)
+            
+            card_time_data.append(data)
+            
+    with open('card_times.csv', 'w') as f:
+        for data in card_time_data:
+            print >>f, ','.join(str(i) for i in data)
+
+
+    stats = process_times(card_time_data)
+    pprint.pprint(stats)
